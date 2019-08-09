66af2fb7c86c0da4e4cd3953d362cb4028854255
timmyru <33581137+timmyru@users.noreply.github.com>
2019-08-09 21:36:47 +0300

    Add files via upload

--- ub1-2019/_header.php
+++ ub1-2019/_header.php
@@ -12,12 +12,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>UB1 | <?=$title ?></title>
-    <link rel="shortcut icon" href="img/favicon.png" type="image/png">
+    <link rel="shortcut icon" href="img/404.png" type="image/png">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox.css?v=2.1.5" media="screen" />
     <link rel="stylesheet" type="text/css" href="fancybox/helpers/jquery.fancybox-buttons.css?v=1.0.5" />
-    <link rel="stylesheet" href="sass/style.css?v=1">
+    <link rel="stylesheet" href="sass/style.css?v=5">
 </head>
 <body>
 
