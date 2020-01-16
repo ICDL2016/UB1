2d83435150d717e2cb20a0977697dc0a89dd2d7d
timmyru <33581137+timmyru@users.noreply.github.com>
2020-01-16 23:22:52 +0300

    Add files via upload

--- ub1-2020-new/_header.php
+++ ub1-2020-new/_header.php
@@ -11,13 +11,23 @@
   <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="sass/style.css">
+  <link rel="stylesheet" href="sass/style.css?v=2">
 </head>
 <body>
   <header class="header">
     <div class="container d-flex justify-content-between align-items-center">
       <div class="header-burger">
-        <i class="fa fa-bars"></i>
+        <div class="header-burger__wrapper">
+          <i class="fa fa-bars burger-icon"></i>
+          <ul class="header-burger__menu">
+            <li><a href="">Главная</a></li>
+            <li><a href="">Первая ссылка</a></li>
+            <li><a href="">Вторая ссылка</a></li>
+            <li><a href="">Третья ссылка</a></li>
+            <li><a href="">Четвертая ссылка</a></li>
+            <li><a href="">Пятая ссылка</a></li>
+          </ul>
+        </div>
         <div class="header-logo">
           <a href="index.php">
             <img src="img/header/logo.png" alt="logo">
@@ -47,7 +57,7 @@
       </div>
       <div class="header-login">
         <a href="login.php">
-          <img src="img/header/login-arrow.png" alt="login">
+          <?php require 'img/header/login.svg'; ?>
         </a>
       </div>
     </div>
--- ub1-2020-new/_scripts.php
+++ ub1-2020-new/_scripts.php
@@ -1,4 +1,4 @@
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="owlcarousel/owl.carousel.min.js"></script>
 <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
-<script src="js/main.js"></script>
+<script src="js/main.js?v=2"></script>
